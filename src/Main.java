43a0eee21e1a8732281b0bd799a24fc4dc6b868d
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-04-24 10:46:31 +0200

    Les cartons sont déménagés

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        int oldPlace = 34;
+        int newPlace = 0;
+        int actualContentTruck = 0;
+        int maxContentTruck = 9;
+        while (oldPlace > 0){
+            if(actualContentTruck == maxContentTruck){
+                newPlace = actualContentTruck;
+                actualContentTruck = 0;
+                System.out.println("There is " + newPlace + "in the new place");
+            }
+            else{
+                actualContentTruck++;
+                oldPlace--;
+                System.out.println("There is " + oldPlace + "in the old place");
+            }
+        }
         System.out.println("Hello world!");
     }
 }
